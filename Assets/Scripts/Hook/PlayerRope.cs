4752cad10280bb0723d5bdb4677c2f61f9d275a8
Tiger Oakes <tigeroakes@gmail.com>
2016-06-02 18:30:37 -0700

    Extending, not overriding.

--- Assets/Scripts/Hook/PlayerRope.cs
+++ Assets/Scripts/Hook/PlayerRope.cs
@@ -3,13 +3,14 @@ using System.Collections.Generic;
 using System;
 
 namespace Hook.Rope {
-	public abstract class PhysicsRope : IRope {	
+	public class PhysicsRope : IRope {	
 		///<summary>The world position of the spring joint anchor</summary>
 		public Vector2 anchor;
 		///<summary>Where the hook connects with the wall</summary>
 		public Vector2 connectedAnchor; 
 		
-		/// <summary>Refers to the Spring Joint used for the rope</summary>
+		///<summary>Refers to the Spring Joint used for the rope</summary>
+		///TODO: Figure out some way to support DistanceJoint2D as well
 		private SpringJoint2D spring; 
 		
 		///<summary>Layers that the hook can collide with</summary>
