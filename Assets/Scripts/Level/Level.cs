7703c1520148d8051d69819896af785cfc41c3ed
Tiger Oakes <tigeroakes@gmail.com>
2016-06-04 14:37:13 -0700

    Adding some JSON loading code

--- Assets/Scripts/Level/Level.cs
+++ Assets/Scripts/Level/Level.cs
@@ -3,5 +3,16 @@ namespace Level {
 	public class Main {
 		public string name;
 		public Platform[] platforms;
+		
+		public Main LoadFile(string url) {
+			StartCoroutine(Text.Load(url, (text) => {
+				Main level = JsonUtility.FromJson<Main>(text);
+				BuildLevel(level);
+			}))
+		}
+		
+		public void BuildLevel(Main level) {
+			
+		}
 	}
 }
