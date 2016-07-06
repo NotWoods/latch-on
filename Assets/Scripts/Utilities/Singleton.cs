b6b8c1321859b4d67b06baf9a727d92adf03dfc4
Tiger Oakes <tigeroakes@gmail.com>
2016-07-05 23:47:14 -0700

    Adding utility scripts

---
+++ Assets/Scripts/Utilities/Singleton.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+
+public class Singleton<T> : MonoBehaviour where T : MonoBehaviour {
+	protected static T sInstance;
+	public static T Instance {
+		get {
+			if (sInstance == null) {
+				sInstance = (T) FindObjectOfType( typeof(T) );
+
+				if (sInstance == null) {
+					Debug.LogError("An instance of " + typeof(T) + 
+					" is needed in the scene, but there is none.");
+				}
+			}
+			return sInstance;
+		}
+	}
+}
