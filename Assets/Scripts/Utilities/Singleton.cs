aeae70eb79c89d0bdf59104eaf3fe3b02e8b009f
Tiger Oakes <tigeroakes@gmail.com>
2016-07-06 21:23:50 -0700

    Moved triangulator class to inside PolygonMesh2D file

--- Assets/Scripts/Utilities/Singleton.cs
+++ Assets/Scripts/Utilities/Singleton.cs
@@ -1,18 +1,20 @@
 ﻿using UnityEngine;
 
-public class Singleton<T> : MonoBehaviour where T : MonoBehaviour {
-	protected static T sInstance;
-	public static T Instance {
-		get {
-			if (sInstance == null) {
-				sInstance = (T) FindObjectOfType( typeof(T) );
-
+namespace Util {
+	public class Singleton<T> : MonoBehaviour where T : MonoBehaviour {
+		protected static T sInstance;
+		public static T Instance {
+			get {
 				if (sInstance == null) {
-					Debug.LogError("An instance of " + typeof(T) + 
-					" is needed in the scene, but there is none.");
+					sInstance = (T) FindObjectOfType( typeof(T) );
+
+					if (sInstance == null) {
+						Debug.LogError("An instance of " + typeof(T) + 
+						" is needed in the scene, but there is none.");
+					}
 				}
+				return sInstance;
 			}
-			return sInstance;
 		}
 	}
 }
