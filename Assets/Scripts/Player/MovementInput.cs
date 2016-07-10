bc3c909de5d6b377704458f257c07fc42c7f71f3
Tiger Oakes <tigeroakes@gmail.com>
2016-07-09 20:57:40 -0700

    Adding movement script code

--- Assets/Scripts/Player/MovementInput.cs
+++ Assets/Scripts/Player/MovementInput.cs
@@ -1,8 +1,10 @@
 using UnityEngine;
 
 namespace Player {
-	public class Movement : MonoBehaviour {
+	public class MovementInput : MonoBehaviour {
 		IMoveable player;
+
+		public float metersPerSecond = 1f;
 
 		void Start() {
 			player = GetComponent<IMoveable>();
@@ -10,7 +12,9 @@ namespace Player {
 
 		void Update() {
 			float horizontal = Input.GetAxis("Horizontal");
-			if (horizontal != 0) player.Drive(horizontal);
+			if (horizontal != 0) {
+				player.Drive(horizontal * metersPerSecond * Time.deltaTime);
+			}
 
 			bool jumpButton = Input.GetButtonDown("Jump");
 			if (jumpButton) player.Jump();
