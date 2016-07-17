e19fa82100c4f3032dc3cfd23877a3078f63909f
Tiger Oakes <tigeroakes@gmail.com>
2016-07-17 15:32:53 -0700

    Re-import merged branch

---
+++ Assets/Scripts/Singleton.cs
@@ -0,0 +1,20 @@
+using UnityEngine;
+
+namespace Util {
+	public class Singleton<T> : MonoBehaviour where T : MonoBehaviour {
+		protected static T sInstance;
+		public static T Instance {
+			get {
+				if (sInstance == null) {
+					sInstance = (T) FindObjectOfType( typeof(T) );
+
+					if (sInstance == null) {
+						Debug.LogError("An instance of " + typeof(T) + 
+						" is needed in the scene, but there is none.");
+					}
+				}
+				return sInstance;
+			}
+		}
+	}
+}
