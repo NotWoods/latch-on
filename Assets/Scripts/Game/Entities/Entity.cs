73a9de9274d0abe7661f86ad8d323759a3b0a2ed
Tiger Oakes <tigeroakes@gmail.com>
2016-12-05 16:56:44 -0800

    Importing to unity

--- Assets/Scripts/Game/Entities/Entity.cs
+++ Assets/Scripts/Game/Entities/Entity.cs
@@ -1,6 +1,5 @@
 using UnityEngine;
-using System.Collections.Generic;
 
-public abstract class Entity : MonoBehaviour, IEntity {
+public class Entity : MonoBehaviour, IEntity {
 	public int ID;
 }
--- Assets/Scripts/Game/Entities/PlayerEntity.cs
+++ Assets/Scripts/Game/Entities/PlayerEntity.cs
@@ -3,20 +3,7 @@ using Prime31;
 
 [RequireComponent (typeof (CharacterController2D))]
 public class PlayerEntity : Entity {
-	public enum State {
-		Idle,   // not moving
-		Walk,   // running along the ground
-		Jump,   // jumping up
-		Hooked, // swinging around on a grappling hook
-		Soar,   // flying after releasing from grappling hook
-		Fall    // falling downward
-	}
-
-	public State CurrentState = State.Idle;
-
 	void Awake() {
-		AssignID();
-		CreateComponent<InputComponent>();
-		CreateComponent<CharacterStatsComponent>();
+
 	}
 }
