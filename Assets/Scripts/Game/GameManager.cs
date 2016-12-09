03d58b374b10fcff3c19727daf6fa07c034cf8ba
Tiger Oakes <tigeroakes@gmail.com>
2016-12-09 13:36:57 -0800

    Added player spawn function

--- Assets/Scripts/Game/GameManager.cs
+++ Assets/Scripts/Game/GameManager.cs
@@ -1,5 +1,29 @@
+using UnityEngine;
 using System.Collections.Generic;
 
 public class GameManager : SingletonMonoBehaviour<GameManager> {
-	public List<PlayerMarker> Players = new List<PlayerMarker>();
+	const string SpawnPointName = "Spawn Point";
+
+	public GameObject PlayerPrefab;
+
+	[SerializeField]
+	private Dictionary<ControlType, GameObject> Players = new Dictionary<ControlType, GameObject>();
+	private Transform spawnPoint;
+
+	void Awake() {
+		spawnPoint = transform.Find(SpawnPointName);
+	}
+
+	public GameObject SpawnPlayer(ControlType controller = ControlType.Keyboard) {
+		if (Players.ContainsKey(controller)) return null;
+
+		GameObject player = Instantiate(PlayerPrefab, spawnPoint.position, Quaternion.identity);
+
+		PlayerMarker marker = player.AddComponent<PlayerMarker>();
+		marker.Controller = controller;
+
+		Players.Add(controller, player);
+		player.GetComponent<Prime31.CharacterController2D>().warpToGrounded();
+		return player;
+	}
 }
