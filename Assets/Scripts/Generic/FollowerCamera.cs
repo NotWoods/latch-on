b62a80cf81ea2d308f96dd9dab69d22c9e85d80e
Tiger Oakes <tigeroakes@gmail.com>
2016-12-09 13:19:05 -0800

    Tweaks to Camera code

--- Assets/Scripts/Generic/FollowerCamera.cs
+++ Assets/Scripts/Generic/FollowerCamera.cs
@@ -1,54 +1,45 @@
 using UnityEngine;
 
 public class FollowerCamera : MonoBehaviour {
-	public Color GizmoColor = new Color(1, 0, 0, 0.1f);
-	/// Area for focus area tracking size
-	public Vector2 FocusAreaSize = new Vector2(3, 6);
-	/// Reference to game object to track
-	public GameObject CameraTarget;
-	/// Camera offset compared to the focus area
-	float zOffset = 30;
-	/// Bounds representing focus area
-	Bounds focusArea;
-	/// The collider of the target game object
-	BoxCollider2D targetCollider;
+	public Vector2 FocusZone = new Vector2(3, 6);
+
+	//[HideInInspector]
+	public Collider2D Target;
+	private Bounds focusBox;
+	private Vector3 cameraOffset;
+	private Color gizmoColor = new Color(1, 0, 0, 0.1f);
 
 	void Start() {
-		if (CameraTarget == null) return;
-
-		targetCollider = CameraTarget.GetComponent<BoxCollider2D>();
-
-		focusArea = new Bounds(targetCollider.bounds.center, FocusAreaSize);
-		float difference = focusArea.min.y - targetCollider.bounds.min.y;
-		focusArea.center -= difference * Vector3.up;
+		focusBox = new Bounds(Vector3.zero, FocusZone);
+		cameraOffset = Vector3.forward * transform.position.z;
 	}
 
 	void LateUpdate() {
-		if (CameraTarget == null) return;
+		focusBox.size = FocusZone;
 
-		Bounds target = targetCollider.bounds;
+		if (Target == null) return;
+		Bounds target = Target.bounds;
 		Vector2 shift = Vector2.zero;
-		if (target.min.x < focusArea.min.x)
-			shift.x = target.min.x - focusArea.min.x;
-		else if (target.max.x > focusArea.max.x)
-			shift.x = target.max.x - focusArea.max.x;
-		if (target.min.y < focusArea.min.y)
-			shift.y = target.min.y - focusArea.min.y;
-		else if (target.max.y > focusArea.max.y)
-			shift.y = target.max.y - focusArea.max.y;
-		focusArea.center += (Vector3) shift;
+
+		if (target.min.x < focusBox.min.x)
+			shift.x = target.min.x - focusBox.min.x;
+		else if (target.max.x > focusBox.max.x)
+			shift.x = target.max.x - focusBox.max.x;
+		if (target.min.y < focusBox.min.y)
+			shift.y = target.min.y - focusBox.min.y;
+		else if (target.max.y > focusBox.max.y)
+			shift.y = target.max.y - focusBox.max.y;
+		focusBox.center += (Vector3) shift;
 
 		transform.position = Vector3.MoveTowards(
 			transform.position,
-			focusArea.center + (zOffset * Vector3.back),
+			focusBox.center + cameraOffset,
 			10
 		);
 	}
 
 	void OnDrawGizmosSelected() {
-		if (CameraTarget == null) return;
-
-		Gizmos.color = GizmoColor;
-		Gizmos.DrawCube(focusArea.center, focusArea.size);
+		Gizmos.color = gizmoColor;
+		Gizmos.DrawCube(focusBox.center, focusBox.size);
 	}
 }
