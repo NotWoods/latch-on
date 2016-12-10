0de048dc31ced5034369c5a75f3405d30c5651ec
Tiger Oakes <tigeroakes@gmail.com>
2016-12-09 18:53:09 -0800

    Added some math code

---
+++ Assets/Scripts/Generic/ExtraMath.cs
@@ -0,0 +1,22 @@
+using UnityEngine;
+
+static class ExtraMath {
+	/// Like Mathf.Sign, but treats 0 seperate from positive numbers.
+	private static int Sign(float number) {
+		if (number < 0) return -1;
+		else if (number > 0) return 1;
+		else return 0;
+	}
+
+	/// <summary>
+	/// Returns the side of the line a point lies on. Result will be -1, 0, or 1.
+	/// Uses determinants to compute.
+	/// </summary>
+	/// <see cref="http://stackoverflow.com/questions/1560492/" />
+	/// <param name="point">Point to check</param>
+	/// <param name="A">Start point of the line</param>
+	/// <param name="B">End point of the line</param>
+	public static int SideOfLine(Vector2 point, Vector2 A, Vector2 B) {
+		return Sign((B.x - A.x) * (point.y - A.y) - (B.y - A.y) * (point.x - A.x));
+	}
+}
