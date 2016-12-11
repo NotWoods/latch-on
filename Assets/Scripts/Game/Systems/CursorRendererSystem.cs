6fef8c9d2f152dac0fd7408b6a71311fef06e669
Tiger Oakes <tigeroakes@gmail.com>
2016-12-11 14:59:27 -0800

    Working on updated cursor system

--- Assets/Scripts/Game/Systems/CursorRendererSystem.cs
+++ Assets/Scripts/Game/Systems/CursorRendererSystem.cs
@@ -1,18 +1,36 @@
 using UnityEngine;
+using UnityEngine.UI;
+using System.Collections.Generic;
 
-public class CursorRendererSystem : EgoSystem<LocalPlayer, Transform, InspectableLineData, InputData, CursorAlias> {
+public class CursorRendererSystem : EgoSystem<LocalPlayer, Transform, InspectableLineData, InputData> {
 	public float PreviewDistance = 2f;
+	public float HighlightScale = 1;
+	public float DarkScale = 0.5f;
+	public Color HighlightColor = Color.white;
+	public Color DarkColor = Color.gray;
+
+	private List<Image> cursors = new List<Image>();
 
 	public override void Update() {
-		ForEachGameObject((ego, p, transform, line, input, cursor) => {
-			RaycastHit2D cursorCheck = Physics2D.Raycast(transform.position, input.PointerDir, PreviewDistance, line.NormalGround);
-			RaycastHit2D hitCheck = Physics2D.Raycast(transform.position, input.PointerDir, line.StartingLength, line.NormalGround);
+		int i = 0;
+		ForEachGameObject((o, p, transform, line, input) => {
+			bool shouldHighlight = Physics2D.Raycast(transform.position,
+				input.PointerDir, line.StartingLength, line.NormalGround);
 
-			cursor.Highlighted = hitCheck ? true : false;
-
-			cursor.Position = cursorCheck
+			RaycastHit2D cursorCheck = Physics2D.Raycast(transform.position,
+				input.PointerDir, PreviewDistance, line.NormalGround);
+			Vector2 position = cursorCheck
 				? cursorCheck.point
 				: (Vector2) transform.position + (input.PointerDir * PreviewDistance);
+
+			Image cursor = cursors[i];
+			cursor.color = Color.Lerp(
+				cursor.color,
+				shouldHighlight ? HighlightColor : DarkColor,
+				Time.deltaTime * 10
+			);
+
+			i++;
 		});
 	}
 }
