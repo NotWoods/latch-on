6d28041b762ff2fc994bd033ca0b30f2ef13b188
Tiger Oakes <tigeroakes@gmail.com>
2017-04-23 21:34:31 -0700

    Removed old methods from LineData

--- Assets/Scripts/Game/Components/Rope/LineData.cs
+++ Assets/Scripts/Game/Components/Rope/LineData.cs
@@ -20,48 +20,6 @@ namespace LatchOn.ECS.Components.Rope {
 		/// True if the rope is anchored to a point
 		public bool IsAnchored = false;
 		/// Point where the rope is anchored. Should be ignored if !IsAnchored
-		public Vector2 AnchorPoint {
-			get { return points[points.Count - 1]; }
-			set { Push(value); }
-		}
-
-		// TODO: Move to wrap component
-		public LayerMask NoHookGround;
-
-		internal Stack<int> MarkedSides = new Stack<int>();
-
-		[SerializeField]
-		private List<Vector2> points = new List<Vector2>();
-
-		public void Push(Vector2 pos) {
-			points.Add(pos);
-		}
-
-		public void Pop() {
-			if (points.Count > 0) {
-				int count = points.Count;
-				CurrentLength += Vector2.Distance(points[count - 1], points[count - 2]);
-				points.RemoveAt(points.Count - 1);
-			}
-			else throw new InvalidOperationException("The LineComponent is empty");
-		}
-
-		public Vector2 Peek() { return points[points.Count - 2]; }
-
-		public bool Anchored() { return points.Count > 0; }
-
-		/// Remove all points from the line
-		public void Clear() { points.Clear(); }
-
-		public IEnumerable<Vector2> Points() {
-			foreach (Vector2 p in points) yield return p;
-		}
-
-		public int Count { get { return points.Count; } }
-
-		public int Side(Vector2 point) {
-			if (Count < 2) throw new InvalidOperationException();
-			return ExtraMath.SideOfLine(point, AnchorPoint, Peek());
-		}
+		public Vector2 AnchorPoint = default(Vector2);
 	}
 }
