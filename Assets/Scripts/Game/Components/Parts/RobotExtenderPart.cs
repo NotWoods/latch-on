95379e396b33457c61aeabbb971fa49c029eb90d
Tiger Oakes <tigeroakes@gmail.com>
2017-06-18 19:37:05 -1000

    Added texture tiling and hiding hands when grappling

--- Assets/Scripts/Game/Components/Parts/RobotExtenderPart.cs
+++ Assets/Scripts/Game/Components/Parts/RobotExtenderPart.cs
@@ -1,10 +1,13 @@
 using UnityEngine;
+using System.Collections.Generic;
 
 namespace LatchOn.ECS.Components.Parts {
 	[DisallowMultipleComponent]
 	public class RobotExtenderPart : MonoBehaviour {
 		public Transform Part;
-
 		public float DefaultScale = 0.18f;
+		public Material PartMaterial;
+
+		public List<Transform> ToHide = new List<Transform>();
 	}
 }
--- Assets/Scripts/Game/Systems/Rendering/ArmExtenderSystem.cs
+++ Assets/Scripts/Game/Systems/Rendering/ArmExtenderSystem.cs
@@ -5,8 +5,24 @@ using LatchOn.ECS.Components.Parts;
 
 namespace LatchOn.ECS.Systems.Rendering {
 	public class ArmExtenderSystem : EgoSystem<WorldPosition, LineData, RobotExtenderPart> {
+		Vector3 HidingPosition = new Vector3(0, 0, -100);
+
 		public override void Update() {
 			ForEachGameObject((ego, position, line, arm) => {
+				bool armsIn = !line.IsAnchored;
+
+				if (armsIn) {
+					CanGrapple grapple;
+					if (ego.TryGetComponents(out grapple)) armsIn = !grapple.DidThrow;
+				}
+
+				foreach (var toHide in arm.ToHide) {
+					toHide.GetComponent<MeshRenderer>().enabled = armsIn;
+					foreach (Transform child in toHide) {
+						child.GetComponent<MeshRenderer>().enabled = armsIn;
+					}
+				}
+
 				if (!line.IsAnchored) {
 					arm.Part.localScale = new Vector3(1, 1, arm.DefaultScale);
 					arm.Part.localRotation = Quaternion.Euler(0, 90, 0);
@@ -18,6 +34,8 @@ namespace LatchOn.ECS.Systems.Rendering {
 
 				arm.Part.localScale = new Vector3(1, 1, distance);
 				arm.Part.LookAt(line.AnchorPoint);
+
+				arm.PartMaterial.mainTextureScale = new Vector2(distance / arm.DefaultScale, 1);
 			});
 		}
 	}
